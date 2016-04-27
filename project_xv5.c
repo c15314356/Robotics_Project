a8b1f6a597f728fe4b1dc35e2e22a255f645ac6a
c15314356 <c15314356@mydit.ie>
2016-04-27 12:39:16 +0100

    Revert "added in display for array"
    This reverts commit d16eddfceb341bd1dfe58c756430ef3d3f5e4d78.

--- project_xv5.c
+++ project_xv5.c
@@ -22,8 +22,7 @@ c15314356
 int blacksq=0;
 int total=0;
 int pos1=0,pos2=0;
-int grid[ROW][COL];
-
+int grid[ROW][COL];//<--------------------- change all intial values to 0
 
 //Turn left 90 degrees
 void LeftTurn(void);
@@ -41,8 +40,6 @@ int NextLineLeft(int direction);
 int NextLineRight(int direction);
 //Go to Start of grid
 void GoStart(void);
-//display array
-void checkarray(void);
 
 task main()
 {
@@ -146,7 +143,7 @@ task main()
             }
         }//end if()
     }//end while()
-		checkarray();
+
     //Goes back to the start function
     GoStart();
 
@@ -337,17 +334,4 @@ void GoStart(void)
      	motor[motorB]=0;
 			motor[motorC]=0;
       wait1Msec(1000);
-}
-void checkarray(void)
-{
-	displayTextLine(2,"%d,%d,%d,%d,%d,%d,%d,%d,%d",grid[6][0],grid[6][1],grid[6][2],grid[6][3],grid[6][4],grid[6][5],grid[6][6],grid[6][7],grid[6][8]);
-	displayTextLine(3,"%d,%d,%d,%d,%d,%d,%d,%d,%d",grid[5][0],grid[5][1],grid[5][2],grid[5][3],grid[5][4],grid[5][5],grid[5][6],grid[5][7],grid[5][8]);
-	displayTextLine(4,"%d,%d,%d,%d,%d,%d,%d,%d,%d",grid[4][0],grid[4][1],grid[4][2],grid[4][3],grid[4][4],grid[4][5],grid[4][6],grid[4][7],grid[4][8]);
-	displayTextLine(5,"%d,%d,%d,%d,%d,%d,%d,%d,%d",grid[3][0],grid[3][1],grid[3][2],grid[3][3],grid[3][4],grid[3][5],grid[3][6],grid[3][7],grid[3][8]);
-	displayTextLine(6,"%d,%d,%d,%d,%d,%d,%d,%d,%d",grid[2][0],grid[2][1],grid[2][2],grid[2][3],grid[2][4],grid[2][5],grid[2][6],grid[2][7],grid[2][8]);
-	displayTextLine(7,"%d,%d,%d,%d,%d,%d,%d,%d,%d",grid[1][0],grid[1][1],grid[1][2],grid[1][3],grid[1][4],grid[1][5],grid[1][6],grid[1][7],grid[1][8]);
-	displayTextLine(8,"%d,%d,%d,%d,%d,%d,%d,%d,%d",grid[0][0],grid[0][1],grid[0][2],grid[0][3],grid[0][4],grid[0][5],grid[0][6],grid[0][7],grid[0][8]);
-	motor[motorB]=0;
-	motor[motorC]=0;
-	wait1Msec(100000);
 }
